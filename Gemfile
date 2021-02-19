b87271ab19f25e0c01bfb0e6d3f840d62b504fb8
EC2 Default User <ec2-user@ip-172-31-84-199.ec2.internal>
2021-02-19 05:30:01 +0000

    add pagination

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ gem 'devise'
 
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
+
+gem 'kaminari','~> 1.2.1'
--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -12,7 +12,7 @@ class PostImagesController < ApplicationController
   end
 
   def index
-      @post_images = PostImage.all
+      @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
